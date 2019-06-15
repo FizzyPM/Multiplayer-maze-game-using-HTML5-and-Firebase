f9ffb0d717b5a97371aeda72e3678a313333dcd7
FizzyPM <munjal.pranav14@gmail.com>
2019-06-15 21:07:58 +0530

    improved structure + final maze generated

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,16 @@
-var game;
+var game = new Phaser.Game(700, 650, Phaser.CANVAS, "");
 var maze = [];
-var mazeWidth = 61;
-var mazeHeight = 51;
-var tileSize = 10;
+var mazeWidth = 35;
+var mazeHeight = 32;
+var tileSize = 20;
 var mazeGraphics;
 
-window.onload = function() {	
-	game = new Phaser.Game(650, 550, Phaser.CANVAS, "");
-     game.state.add("PlayGame",playGame);
-     game.state.start("PlayGame");
-}
-posx=1;
-posy=1;
-var playGame = function(game){};
-
-playGame.prototype = {
+var GameState = {
+     preload: function(){
+     //    this.load.image('background', 'assets/images/background2.png');
+     },
      create: function(){
-          game.physics.startSystem(Phaser.Physics.ARCADE);
-          cursors = game.input.keyboard.createCursorKeys();
+          // console.log("create");
           mazeGraphics = game.add.graphics(0, 0);
           var moves = [];
           for(var i = 0; i < mazeHeight; i ++){
@@ -29,74 +22,72 @@ playGame.prototype = {
           var posX = 1;
           var posY = 1;
           maze[posX][posY] = 0; 
-          moves.push(posY + posY * mazeWidth);
-          game.time.events.loop(Phaser.Timer.SECOND/100, function(){
-               if(moves.length){       
-                    var possibleDirections = "";
-                    if(posX+2 > 0 && posX + 2 < mazeHeight - 1 && maze[posX + 2][posY] == 1){
-                         possibleDirections += "S";
+          moves.push(posY + posY * mazeWidth);          
+          while(moves.length){       
+               var possibleDirections = "";
+               if(posX+2 > 0 && posX + 2 < mazeHeight - 1 && maze[posX + 2][posY] == 1){
+                    possibleDirections += "S";
+               }
+               if(posX-2 > 0 && posX - 2 < mazeHeight - 1 && maze[posX - 2][posY] == 1){
+                    possibleDirections += "N";
+               }
+               if(posY-2 > 0 && posY - 2 < mazeWidth - 1 && maze[posX][posY - 2] == 1){
+                    possibleDirections += "W";
+               }
+               if(posY+2 > 0 && posY + 2 < mazeWidth - 1 && maze[posX][posY + 2] == 1){
+                    possibleDirections += "E";
+               } 
+               if(possibleDirections){
+                    var move = game.rnd.between(0, possibleDirections.length - 1);
+                    switch (possibleDirections[move]){
+                         case "N": 
+                              maze[posX - 2][posY] = 0;
+                              maze[posX - 1][posY] = 0;
+                              posX -= 2;
+                              break;
+                         case "S":
+                              maze[posX + 2][posY] = 0;
+                              maze[posX + 1][posY] = 0;
+                              posX += 2;
+                              break;
+                         case "W":
+                              maze[posX][posY - 2] = 0;
+                              maze[posX][posY - 1] = 0;
+                              posY -= 2;
+                              break;
+                         case "E":
+                              maze[posX][posY + 2]=0;
+                              maze[posX][posY + 1]=0;
+                              posY += 2;
+                              break;         
                     }
-                    if(posX-2 > 0 && posX - 2 < mazeHeight - 1 && maze[posX - 2][posY] == 1){
-                         possibleDirections += "N";
-                    }
-                    if(posY-2 > 0 && posY - 2 < mazeWidth - 1 && maze[posX][posY - 2] == 1){
-                         possibleDirections += "W";
-                    }
-                    if(posY+2 > 0 && posY + 2 < mazeWidth - 1 && maze[posX][posY + 2] == 1){
-                         possibleDirections += "E";
-                    } 
-                    if(possibleDirections){
-                         var move = game.rnd.between(0, possibleDirections.length - 1);
-                         switch (possibleDirections[move]){
-                              case "N": 
-                                   maze[posX - 2][posY] = 0;
-                                   maze[posX - 1][posY] = 0;
-                                   posX -= 2;
-                                   break;
-                              case "S":
-                                   maze[posX + 2][posY] = 0;
-                                   maze[posX + 1][posY] = 0;
-                                   posX += 2;
-                                   break;
-                              case "W":
-                                   maze[posX][posY - 2] = 0;
-                                   maze[posX][posY - 1] = 0;
-                                   posY -= 2;
-                                   break;
-                              case "E":
-                                   maze[posX][posY + 2]=0;
-                                   maze[posX][posY + 1]=0;
-                                   posY += 2;
-                                   break;         
-                         }
-                         moves.push(posY + posX * mazeWidth);     
-                    }
-                    else{
-                         var back = moves.pop();
-                         posX = Math.floor(back / mazeWidth);
-                         posY = back % mazeWidth;
-                    }
-                    drawMaze(posX, posY);                                         
-               }      
-          }, this);	  
+                    moves.push(posY + posX * mazeWidth);     
+               }
+               else{
+                    var back = moves.pop();
+                    posX = Math.floor(back / mazeWidth);
+                    posY = back % mazeWidth;
+               }                                 
+          }     
+          this.drawMaze(posX, posY)
      },
      update: function(){
-
-     }
-}
-
-function drawMaze(posX, posY){
-     mazeGraphics.clear();
-     mazeGraphics.beginFill(0xcccccc);
-     for(i = 0; i < mazeHeight; i ++){
-          for(j = 0; j < mazeWidth; j ++){
-               if(maze[i][j] == 1){
-                    mazeGraphics.drawRect(j * tileSize, i * tileSize, tileSize, tileSize);                 
+     },
+     drawMaze: function(){
+          // console.log('draw');
+          mazeGraphics.clear();
+          mazeGraphics.beginFill(0xcccccc);
+          for(i = 0; i < mazeHeight; i ++){
+               for(j = 0; j < mazeWidth; j ++){
+                    if(maze[i][j] == 1){
+                         mazeGraphics.drawRect(j * tileSize, i * tileSize, tileSize, tileSize);                 
+                    }
                }
           }
+          mazeGraphics.endFill();
      }
-     mazeGraphics.endFill();   
-     mazeGraphics.beginFill(0xff0000); 
-     mazeGraphics.drawRect(posY * tileSize, posX * tileSize, tileSize, tileSize);
-     mazeGraphics.endFill();   
 }
+game.state.add('GameState', GameState);
+game.state.start('GameState');
+
+
